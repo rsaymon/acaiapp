62514be9ab6a716402dc395bd44036e366bdcbff
Ryan <rsaymon@hotmail.com>
2021-04-08 14:34:08 -0300

    tela de login finalizada

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,22 +1,69 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 
-import {Image} from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import Icon from 'react-native-vector-icons/Feather';
 
-import {Container, Title} from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
     <>
-      <Container>
-        <Image source={require('../../assets/logotuira.png')} />
-        <Title>Faça seu login</Title>
+      {/* KeyboardAvoidingView diminuir tamanho ao abrir teclado */}
+      <KeyboardAvoidingView
+        style={{flex: 1}}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled>
+        {/* ScrollView dá rolagem. PersistTaps -> teclado ao clicar em qualquer canto da tela*/}
+        <ScrollView
+          keyboardShouldPersistTaps={'handled'}
+          contentContainerStyle={{flex: 1}}>
+          <Container>
+            <Image source={require('../../assets/logotuira.png')} />
+            {/* View por volta do texto para que o texto tenha estilização*/}
+            <View>
+              <Title>Faça seu login</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button
+              onPress={() => {
+                console.log('deu');
+              }}>
+              Entrar
+            </Button>
 
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" icon="lock" placeholder="Senha" />
-        <Button>Entrar</Button>
-      </Container>
+            <ForgotPassword
+              onPress={() => {
+                console.log('deu');
+              }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton
+        onPress={() => {
+          console.log('deu');
+        }}>
+        <Icon name="log-in" size={20} color="#f4ede8" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
     </>
   );
 };
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import {getBottomSpace} from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -8,9 +9,41 @@ export const Container = styled.View`
   padding: 0 30px;
 `;
 
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
 export const Title = styled.Text`
   font-size: 20px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
   margin: 64px 0 24px;
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #35350e;
+  border-top-width: 1px;
+  border-color: #353500;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #f4ede8;
+  font-size: 14px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
